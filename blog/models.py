9c2ffd85f51b1f5a7863e1e8f29d82c7c1b7aa19
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-01-09 19:09:19 +0100

    feat: create a database model for the blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+STATUS = ((0, "Draft"), (1, "Published"))
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+    )
+    status = models.IntegerField(choices=STATUS, default=0)
