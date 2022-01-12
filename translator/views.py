3cd69750a1d7bcb74f55e1819f5af54500308b0d
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-01-12 13:46:55 +0100

    feat: get and proccess user input in the translator form

--- translator/views.py
+++ translator/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def translator_view(request):
-    return render(request, "translator.html")
+    if request.method == "POST":
+        origin_text = request.POST["origin_text"]
+        translated_text = origin_text.upper()
+        return render(
+            request,
+            "translator.html",
+            {"origin_text": origin_text, "translated_text": translated_text},
+        )
+    else:
+        return render(request, "translator.html")
