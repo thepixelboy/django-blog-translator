89fb95589b162a012f900124779085d2a2549283
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-01-12 14:03:38 +0100

    feat:  complete the translator app

--- translator/views.py
+++ translator/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
+from . import translate
+
 
 # Create your views here.
 def translator_view(request):
     if request.method == "POST":
         origin_text = request.POST["origin_text"]
-        translated_text = origin_text.upper()
+        translated_text = translate.translate(origin_text)
         return render(
             request,
             "translator.html",
